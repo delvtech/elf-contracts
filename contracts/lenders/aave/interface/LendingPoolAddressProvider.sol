b022536fb69e387320b11797dac03e34da150e16
wilbarnes <wbarnesf@gmail.com>
2020-10-06 20:03:06 -0400

    removing old files

--- contracts/lenders/aave/interface/LendingPoolAddressProvider.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.5.8 <0.8.0;
-
-interface LendingPoolAddressesProvider {
-    function getLendingPool() external view returns (address);
-
-    function getLendingPoolCore() external view returns (address);
-
-    function getPriceOracle() external view returns (address);
-}

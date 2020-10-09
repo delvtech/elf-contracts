d1a8528a5e31132e0bb8f809be6463f3f8fd6d54
jonny rhea <jonathan.rhea@gmail.com>
2020-10-08 23:51:57 -0500

    modify ci to check fmt

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ all    :; clean build test
 build  :; dapp build
 init   :; dapp update; npm install
 fmt    :; npm run prettier
+fmt-ci :; npm run style-check
 lint   :; npm run solhint
 clean  :; dapp clean
 test   :; dapp build; hevm dapp-test --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
--- src/libraries/Address.sol
+++ src/libraries/Address.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.16;
 library Address {
     function isContract(address account) internal view returns (bool) {
         bytes32 codehash;
+        // prettier-ignore
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         // solhint-disable-next-line no-inline-assembly
         assembly { codehash := extcodehash(account) }

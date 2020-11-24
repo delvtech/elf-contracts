b383559ccc0d9ef0c71b9921a47de059f54d7eaf
Matthew Brown <mattfdbrown@gmail.com>
2020-11-24 08:23:40 -0800

    non abstract contracts cannot have internal constructors

--- src/interfaces/Context.sol
+++ src/interfaces/Context.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.5.16;
 
 contract Context {
-    constructor() internal {}
+    constructor() {}
 
     function _msgSender() internal view returns (address payable) {
         return msg.sender;

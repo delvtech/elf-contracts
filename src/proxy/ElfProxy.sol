b2e2469d17aa9310a76694fdc44cdc00b17b1047
Matthew Brown <mattfdbrown@gmail.com>
2020-11-25 15:04:08 -0800

    fix apy endpoint (#50)

--- src/proxy/ElfProxy.sol
+++ src/proxy/ElfProxy.sol
@@ -32,7 +32,7 @@ contract ElfProxy {
 
     function getPoolAPY(address payable _pool) external pure returns (uint256) {
         require(_pool != address(0));
-        return 125 * 10**18;
+        return 2.13 * 10**18;
     }
 
     function getNumPoolAllocations(address payable _pool)

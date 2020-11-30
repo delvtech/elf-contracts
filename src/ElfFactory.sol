31581d4fdbf51e2f567130dcaa7c352f891cf5a9
jonny rhea <jonathan.rhea@gmail.com>
2020-11-30 15:51:05 -0600

    fmt

--- src/ElfFactory.sol
+++ src/ElfFactory.sol
@@ -4,23 +4,15 @@ pragma solidity >=0.5.8 <0.8.0;
 import "./Elf.sol";
 
 contract ElfFactory {
-    event LOG_NEW_POOL(
-        address indexed caller,
-        address indexed pool
-    );
+    event LOG_NEW_POOL(address indexed caller, address indexed pool);
 
-    mapping(address=>bool) private _isPool;
+    mapping(address => bool) private _isPool;
 
-    function isPool(address p)
-        external view returns (bool)
-    {
+    function isPool(address p) external view returns (bool) {
         return _isPool[p];
     }
 
-    function newPool(address payable _weth)
-        external
-        returns (Elf)
-    {
+    function newPool(address payable _weth) external returns (Elf) {
         Elf _pool = new Elf(_weth);
         ElfAllocator _allocator = new ElfAllocator(address(_pool), _weth);
         _pool.setAllocator(payable(_allocator));

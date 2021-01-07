8837e95bb285000b2de80b5d7bbc25f5b914cdce
Nicholas Evans <nicholas.evans@thesis.co>
2021-01-06 18:04:11 -0600

    Deploy YVaultAssetProxy during new pool creation
    Currently creating a new pool requires multiple
    transactions without a wrapper.
    Instead of the `YVaultAssetProxy` deployment being a prerequisite to
    calling `factory.newPool`, the `YVaultAssetProxy` is deployed
    during the `factory.newPool` execution.

--- src/ElfFactory.sol
+++ src/ElfFactory.sol
@@ -3,8 +3,10 @@ pragma solidity >=0.5.8 <0.8.0;
 
 import "./Elf.sol";
 import "./assets/interface/IAssetProxy.sol";
+import "./assets/YVaultAssetProxy.sol";
 
 contract ElfFactory {
+
     event NewPool(address indexed caller, address indexed pool);
 
     mapping(address => bool) private _isPool;
@@ -13,10 +15,10 @@ contract ElfFactory {
         return _isPool[p];
     }
 
-    function newPool(address token, address proxy) external returns (Elf) {
-        IAssetProxy _proxy = IAssetProxy(proxy);
-        Elf _pool = new Elf(token, _proxy.vault(), address(_proxy));
-        _pool.setGovernance(msg.sender);
+    function newPool(address token, address vault) external returns (Elf) {
+        YVaultAssetProxy _proxy = new YVaultAssetProxy(vault, token);
+        Elf _pool = new Elf(token, address(_proxy.vault()), address(_proxy));
+        _proxy.setPool(address(_pool));
         _isPool[address(_pool)] = true;
         emit NewPool(msg.sender, address(_pool));
         return _pool;

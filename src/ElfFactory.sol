20d8f9827edf8b8942f929cd9c17f6dfb2b638be
Nicholas Evans <nicholas.evans@thesis.co>
2021-01-07 11:41:08 -0600

    Update ElfFactory to work with CloneFactory
    ALC initialization and contract initializations
    occur during new pool creation.
    
    - Added a constructor here to initialize the master
    Elf contract and Asset proxy

--- src/ElfFactory.sol
+++ src/ElfFactory.sol
@@ -2,24 +2,44 @@
 pragma solidity >=0.5.8 <0.8.0;
 
 import "./Elf.sol";
+import "./CloneFactory.sol";
 import "./assets/interface/IAssetProxy.sol";
 import "./assets/YVaultAssetProxy.sol";
 
-contract ElfFactory {
-    event NewPool(address indexed caller, address indexed pool);
+contract ElfFactory is CloneFactory {
+    address public masterAssetProxy;
+    address public masterElf;
+
+    event PoolCloneCreated(
+        address indexed caller,
+        address indexed pool
+    );
 
     mapping(address => bool) private _isPool;
+
+    constructor(address _masterAssetProxy, address _masterElf) public {
+        masterAssetProxy = _masterAssetProxy;
+        masterElf = _masterElf;
+    }
 
     function isPool(address p) external view returns (bool) {
         return _isPool[p];
     }
 
     function newPool(address token, address vault) external returns (Elf) {
-        YVaultAssetProxy _proxy = new YVaultAssetProxy(vault, token);
-        Elf _pool = new Elf(token, address(_proxy.vault()), address(_proxy));
-        _proxy.setPool(address(_pool));
-        _isPool[address(_pool)] = true;
-        emit NewPool(msg.sender, address(_pool));
-        return _pool;
+        address proxyClone = createClone(masterAssetProxy);
+        address elfClone = createClone(masterElf);
+        YVaultAssetProxy proxy = YVaultAssetProxy(proxyClone);
+        Elf pool = Elf(elfClone);
+
+        proxy.initialize(address(this));
+        proxy.initializeProxy(vault, token, elfClone);
+
+        pool.initialize(address(this));
+        pool.initializeElf(token, address(proxy.vault()), proxyClone);
+
+        _isPool[address(pool)] = true;
+        emit PoolCloneCreated(msg.sender, elfClone);
+        return pool;
     }
 }

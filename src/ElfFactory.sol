da7686558bfc653ec9eef35282e0b540c199e4eb
andy8052 <andychorlian@gmail.com>
2021-01-19 11:28:27 -0500

    Elf and Elf Factory contract cleanup

--- src/ElfFactory.sol
+++ src/ElfFactory.sol
@@ -4,21 +4,31 @@ pragma solidity >=0.5.8 <0.8.0;
 import "./Elf.sol";
 import "./assets/interface/IAssetProxy.sol";
 
+/// @author Element Finance
+/// @title Elf Factory
+/// @dev contract is due for a rewrite
 contract ElfFactory {
     event NewPool(address indexed caller, address indexed pool);
 
     mapping(address => bool) private _isPool;
 
-    function isPool(address p) external view returns (bool) {
-        return _isPool[p];
+    /// @notice verified if an address is an Elf Pool
+    /// @param _p the address to verify
+    /// @return boolean of whether or not it is a pool
+    function isPool(address _p) external view returns (bool) {
+        return _isPool[_p];
     }
 
-    function newPool(address token, address proxy) external returns (Elf) {
-        IAssetProxy _proxy = IAssetProxy(proxy);
-        Elf _pool = new Elf(token, _proxy.vault(), address(_proxy));
-        _pool.setGovernance(msg.sender);
-        _isPool[address(_pool)] = true;
-        emit NewPool(msg.sender, address(_pool));
-        return _pool;
+    /// @notice make a new Elf pool
+    /// @param _token the underlying token for an Elf Core contract
+    /// @param _proxy the proxy address for the vault
+    /// @return the address of the new Elf pool contract
+    function newPool(address _token, address _proxy) external returns (Elf) {
+        IAssetProxy proxy = IAssetProxy(_proxy);
+        Elf pool = new Elf(_token, proxy.vault(), address(proxy));
+        pool.setGovernance(msg.sender);
+        _isPool[address(pool)] = true;
+        emit NewPool(msg.sender, address(pool));
+        return pool;
     }
 }

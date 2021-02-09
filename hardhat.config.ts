0a8a9f718787593f20a6b38e32ad43a5f006f3ba
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-09 12:49:14 -0600

    hardhat init

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-typechain";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+// hre is provided on the global scope.  We cannot import it here or in any file we import since we
+// cannot load the hre while doing configuration.
+declare global {
+  var hre: HardhatRuntimeEnvironment;
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.0",
+};
+
+export default config;

4bcd4261f31c9e4c37f81c1cdcb1cafda0e9330c
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-10 02:25:27 -0600

    Copy relevant contracts

---
+++ contracts/assets/interface/IAssetProxy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+interface IAssetProxy {
+    function vault() external view returns (address);
+
+    function deposit() external;
+
+    function withdraw() external;
+
+    function underlying(uint256) external view returns (uint256);
+}

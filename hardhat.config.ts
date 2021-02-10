8b19be564c81b06583270282ba490a3704963cd7
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-10 13:51:15 -0600

    Remove hre declaration
    hre is provided in args to task if needed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,7 @@
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
 
-import {HardhatUserConfig, task} from "hardhat/config";
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-
-// hre is provided on the global scope.  We cannot import it here or in any file we import since we
-// cannot load the hre while doing configuration.
-declare global {
-  var hre: HardhatRuntimeEnvironment;
-}
+import {HardhatUserConfig} from "hardhat/config";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",

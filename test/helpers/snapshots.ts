1018c573df944a7fc54594a2a4e4e44e2403aa94
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-11 03:39:24 -0600

    Add snapshot functionality
    Snapshots allow for easy state control

---
+++ test/helpers/snapshots.ts
@@ -0,0 +1,16 @@
+import {MockProvider} from "ethereum-waffle";
+
+const snapshotIdStack: number[] = [];
+export const createSnapshot = async (provider: MockProvider) => {
+  let id = await provider.send("evm_snapshot", []);
+  snapshotIdStack.push(id);
+};
+
+export const restoreSnapshot = async (provider: MockProvider) => {
+  const id = snapshotIdStack.pop();
+  try {
+    await provider.send("evm_revert", [id]);
+  } catch (ex) {
+    throw new Error(`Snapshot with id #${id} failed to revert`);
+  }
+};

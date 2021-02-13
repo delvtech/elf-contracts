95471029e97f6b54f7abcf669d738f8021f8a87f
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-13 17:06:39 -0600

    Deploy more contracts, Use TS factory
    Deploy the Tranch and ElfStub contracts,
    add yc to the export object
    
    Use the Typescript factories to get deployed contracts.

--- test/helpers/deployer.ts
+++ test/helpers/deployer.ts
@@ -1,9 +1,16 @@
 import {Elf} from "../../typechain/Elf";
+import {ElfStub} from "../../typechain/ElfStub";
+import {Tranche} from "../../typechain/Tranche";
+import {YC} from "../../typechain/YC";
 import {ElfFactory} from "../../typechain/ElfFactory";
 import {AToken} from "../../typechain/AToken";
 import {AYVault} from "../../typechain/AYVault";
 import {YVaultAssetProxy} from "../../typechain/YVaultAssetProxy";
-import ElfArtifact from "../../artifacts/contracts/Elf.sol/Elf.json";
+import {YearnVault} from "../../typechain/YearnVault";
+
+import {YearnVault__factory} from "../../typechain/factories/YearnVault__factory";
+import {Elf__factory} from "../../typechain/factories/Elf__factory";
+import {YC__factory} from "../../typechain/factories/YC__factory";
 
 import {Signer} from "ethers";
 import {ethers} from "hardhat";
@@ -14,12 +21,30 @@ export interface fixtureInterface {
   usdc: AToken;
   yusdc: AYVault;
   yusdcAsset: YVaultAssetProxy;
+  yusdcAssetVault: YearnVault;
   elf: Elf;
+  elfStub: ElfStub;
+  tranche: Tranche;
+  yc: YC;
 }
 
 const deployElfFactory = async (signer: Signer) => {
   const deployer = await ethers.getContractFactory("ElfFactory", signer);
   return (await deployer.deploy()) as ElfFactory;
+};
+
+const deployElfStub = async (signer: Signer) => {
+  const deployer = await ethers.getContractFactory("ElfStub", signer);
+  return (await deployer.deploy()) as ElfStub;
+};
+
+const deployTranche = async (
+  signer: Signer,
+  elfAddress: string,
+  lockDuration: number
+) => {
+  const deployer = await ethers.getContractFactory("Tranche", signer);
+  return (await deployer.deploy(elfAddress, lockDuration)) as Tranche;
 };
 
 const deployUsdc = async (signer: Signer, owner: string) => {
@@ -44,6 +69,12 @@ const deployYusdcAsset = async (
 export async function loadFixture() {
   const [signer] = await ethers.getSigners();
   const signerAddress = (await signer.getAddress()) as string;
+  const elfStub = (await deployElfStub(signer)) as ElfStub;
+  const tranche = (await deployTranche(
+    signer,
+    elfStub.address,
+    5000000
+  )) as Tranche;
   const elfFactory = (await deployElfFactory(signer)) as ElfFactory;
   const usdc = (await deployUsdc(signer, signerAddress)) as AToken;
   const yusdc = (await deployYusdc(signer, usdc.address)) as AYVault;
@@ -53,15 +84,28 @@ export async function loadFixture() {
     usdc.address
   )) as YVaultAssetProxy;
 
+  const vaultAddress = await yusdcAsset.vault();
+  const yusdcAssetVault = YearnVault__factory.connect(vaultAddress, signer);
+
+  const ycAddress = await tranche.yc();
+  const yc = YC__factory.connect(ycAddress, signer);
+
   await elfFactory.newPool(usdc.address, yusdcAsset.address);
 
   const filter = await elfFactory.filters.NewPool(null, null);
   const event = await elfFactory.queryFilter(filter);
-  const elf = new ethers.Contract(
-    event[0].args?.pool,
-    ElfArtifact.abi,
-    ethers.provider
-  ) as Elf;
+  const elf = Elf__factory.connect(event[0].args?.pool, signer);
 
-  return {signer, elfFactory, usdc, yusdc, yusdcAsset, elf};
+  return {
+    signer,
+    elfFactory,
+    usdc,
+    yusdc,
+    yusdcAsset,
+    yusdcAssetVault,
+    elf,
+    elfStub,
+    tranche,
+    yc,
+  };
 }

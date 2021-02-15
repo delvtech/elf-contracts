3f35afe0950a44d7a42fa769464de19a529196a2
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-15 02:36:17 -0600

    Add 2 more fixture loaders:
    One fixture for ETH pool mainnet tests and one for USDC
    pool mainnet tests.
    These are separated due to the slight differences in interface
    between WETH and USDC

--- test/helpers/deployer.ts
+++ test/helpers/deployer.ts
@@ -7,10 +7,14 @@ import {AToken} from "../../typechain/AToken";
 import {AYVault} from "../../typechain/AYVault";
 import {YVaultAssetProxy} from "../../typechain/YVaultAssetProxy";
 import {YearnVault} from "../../typechain/YearnVault";
+import {IWETH} from "../../typechain/IWETH";
+import {IERC20} from "../../typechain/IERC20";
 
 import {YearnVault__factory} from "../../typechain/factories/YearnVault__factory";
 import {Elf__factory} from "../../typechain/factories/Elf__factory";
 import {YC__factory} from "../../typechain/factories/YC__factory";
+import {IWETH__factory} from "../../typechain/factories/IWETH__factory";
+import {IERC20__factory} from "../../typechain/factories/IERC20__factory";
 
 import {Signer} from "ethers";
 import {ethers} from "hardhat";
@@ -26,6 +30,22 @@ export interface fixtureInterface {
   elfStub: ElfStub;
   tranche: Tranche;
   yc: YC;
+}
+
+export interface ethPoolMainnetInterface {
+  signer: Signer;
+  weth: IWETH;
+  yweth: YearnVault;
+  ywethAsset: YVaultAssetProxy;
+  elf: Elf;
+}
+
+export interface usdcPoolMainnetInterface {
+  signer: Signer;
+  usdc: IERC20;
+  yusdc: YearnVault;
+  yusdcAsset: YVaultAssetProxy;
+  elf: Elf;
 }
 
 const deployElfFactory = async (signer: Signer) => {
@@ -107,5 +127,63 @@ export async function loadFixture() {
     elfStub,
     tranche,
     yc,
+  };
+}
+
+export async function loadEthPoolMainnetFixture() {
+  const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const ywethAddress = "0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7";
+  const [signer] = await ethers.getSigners();
+  const elfFactory = (await deployElfFactory(signer)) as ElfFactory;
+
+  const weth = IWETH__factory.connect(wethAddress, signer);
+  const yweth = YearnVault__factory.connect(ywethAddress, signer);
+  const ywethAsset = (await deployYusdcAsset(
+    signer,
+    yweth.address,
+    weth.address
+  )) as YVaultAssetProxy;
+
+  await elfFactory.newPool(weth.address, ywethAsset.address);
+
+  const filter = await elfFactory.filters.NewPool(null, null);
+  const event = await elfFactory.queryFilter(filter);
+  const elf = Elf__factory.connect(event[0].args?.pool, signer);
+
+  return {
+    signer,
+    weth,
+    yweth,
+    ywethAsset,
+    elf,
+  };
+}
+
+export async function loadUsdcPoolMainnetFixture() {
+  const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const yusdcAddress = "0x597aD1e0c13Bfe8025993D9e79C69E1c0233522e";
+  const [signer] = await ethers.getSigners();
+  const elfFactory = (await deployElfFactory(signer)) as ElfFactory;
+
+  const usdc = IERC20__factory.connect(usdcAddress, signer);
+  const yusdc = YearnVault__factory.connect(yusdcAddress, signer);
+  const yusdcAsset = (await deployYusdcAsset(
+    signer,
+    yusdc.address,
+    usdc.address
+  )) as YVaultAssetProxy;
+
+  await elfFactory.newPool(usdc.address, yusdcAsset.address);
+
+  const filter = await elfFactory.filters.NewPool(null, null);
+  const event = await elfFactory.queryFilter(filter);
+  const elf = Elf__factory.connect(event[0].args?.pool, signer);
+
+  return {
+    signer,
+    usdc,
+    yusdc,
+    yusdcAsset,
+    elf,
   };
 }

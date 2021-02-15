fc2cdd10578cffe24f1f2aa57c67b25c9b0c7e58
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-15 02:31:33 -0600

    Update hardhat config:
    set up forking with default fork block at 11853372
    set accounts generated to 4 to speed up tests
    increase default ETH allocated to 100k

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,19 @@ import {HardhatUserConfig} from "hardhat/config";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url:
+          "https://eth-mainnet.alchemyapi.io/v2/kwjMP-X-Vajdk1ItCfU-56Uaq1wwhamK",
+        blockNumber: 11853372,
+      },
+      accounts: {
+        accountsBalance: "100000000000000000000000", // 100000 ETH
+        count: 4,
+      },
+    },
+  },
   solidity: "0.8.0",
 };
 

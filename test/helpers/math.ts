e65e232a80593ff4b728ab862931dde39b5d2ec2
Nicholas Evans <nicholas.evans@thesis.co>
2021-02-16 09:53:21 -0600

    round BigNumber input
    Must be round to avoid floating point input

--- test/helpers/math.ts
+++ test/helpers/math.ts
@@ -2,5 +2,7 @@ import {ethers} from "hardhat";
 import {BigNumber} from "ethers";
 
 export const bnFloatMultiplier = (number: BigNumber, mulpiplier: number) => {
-  return ethers.BigNumber.from((number.toNumber() * mulpiplier).toString());
+  return ethers.BigNumber.from(
+    Math.round(number.toNumber() * mulpiplier).toString()
+  );
 };

8d1cbe95fcb43997b3b249d09e03a2542c33a1ca
Nicholas Evans <nicholas.evans@thesis.co>
2021-03-06 06:46:46 -0600

    further tweak optimizer runs
    8500 -> 7500. Adding tranche address
    deriving to the TrancheFactory
    increases bytecode size.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 8500,
+            runs: 7500,
           },
         },
       },

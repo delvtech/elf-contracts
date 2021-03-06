e1771196af3f369d18d568632b0f0c2fb06b6679
Nicholas Evans <nicholas.evans@thesis.co>
2021-03-06 04:56:13 -0600

    Adjust optimizer runs
    Temporarily tweak adjuster runs to get in the bytecode size limit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 10000,
+            runs: 8500,
           },
         },
       },

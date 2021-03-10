7728d3e05348baaab5be6347bb9a3f6b286c406c
andy8052 <andy8052@protonmail.com>
2021-03-10 14:58:47 -0500

    up runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 1000,
+            runs: 7500,
           },
         },
       },

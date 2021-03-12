dc41e2459e140bdf51c67c9751856b0838e34c3b
Matthew Brown <mattfdbrown@gmail.com>
2021-03-12 10:12:03 -0800

    Fix onSwap calls for ConvergentCurvePool (#140)

---
+++ test/helpers/deployConvergentCurvePool.ts
@@ -0,0 +1,56 @@
+import { Signer } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import { THIRTY_DAYS_IN_SECONDS } from "test/helpers/time";
+import { ConvergentCurvePoolTest__factory } from "typechain/factories/ConvergentCurvePoolTest__factory";
+import { TestERC20 } from "typechain/TestERC20";
+import { Vault } from "typechain/Vault";
+
+const defaultOptions = {
+  swapFee: ".003",
+  durationInSeconds: THIRTY_DAYS_IN_SECONDS,
+};
+
+export async function deployConvergentCurvePool(
+  signer: Signer,
+  vaultContract: Vault,
+  baseAssetContract: TestERC20,
+  yieldAssetContract: TestERC20,
+  options?: {
+    swapFee: string;
+    expiration: number;
+    durationInSeconds: number;
+  }
+) {
+  const { expiration: providedExpiration, swapFee, durationInSeconds } = {
+    ...defaultOptions,
+    ...options,
+  };
+  const elementAddress = await signer.getAddress();
+  const baseAssetSymbol = await baseAssetContract.symbol();
+  const curvePoolDeployer = new ConvergentCurvePoolTest__factory(signer);
+
+  const dateInMilliseconds = Date.now();
+  const dateInSeconds = dateInMilliseconds / 1000;
+  const defaultExpiration = Math.round(dateInSeconds + durationInSeconds);
+  const expiration = providedExpiration ?? defaultExpiration;
+
+  const poolContract = await curvePoolDeployer.deploy(
+    baseAssetContract.address,
+    yieldAssetContract.address,
+    expiration,
+    durationInSeconds,
+    vaultContract.address,
+    parseEther(swapFee),
+    elementAddress,
+    `Element ${baseAssetSymbol} - fy${baseAssetSymbol}`,
+    `${baseAssetSymbol}-fy${baseAssetSymbol}`
+  );
+
+  // grab last poolId from last event
+  const newPools = vaultContract.filters.PoolCreated(null);
+  const results = await vaultContract.queryFilter(newPools);
+  const poolIds: string[] = results.map((result) => result.args?.poolId);
+  const poolId = poolIds[poolIds.length - 1];
+
+  return { poolId, poolContract };
+}
---
+++ test/helpers/initializeConvergentCurvePool.ts
@@ -0,0 +1,65 @@
+import { BigNumber, ethers, Signer } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import { TestERC20 } from "typechain";
+import { Tranche } from "typechain/Tranche";
+import { Vault } from "typechain/Vault";
+
+/**
+ * the erc20 allowance() method takes a unit256, therefore the max you can approve is 2^256 - 1
+ */
+const MAX_ALLOWANCE = BigNumber.from(
+  "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+);
+/**
+ * Stakes an initial amount of base asset into the YieldPool
+ *
+ * @param poolId
+ * @param elementSigner
+ * @param vaultContract
+ * @param baseAssetContract
+ * @param trancheContract
+ * @param amountIn
+ */
+export async function initializeConvergentCurvePool(
+  poolId: string,
+  elementSigner: Signer,
+  vaultContract: Vault,
+  baseAssetContract: TestERC20,
+  trancheContract: Tranche,
+  amountIn: string
+) {
+  const elementAddress = await elementSigner.getAddress();
+  const { tokens } = await vaultContract.getPoolTokens(poolId);
+
+  // const parseToken = (value: string) => parseUnits(value, baseAssetDecimals);
+  // TODO: double check if these should be normalized to Ether or not.  I think balancer wants
+  // everything in 18 decimal format so leaving this as parseEther.  If not, then we'll have to use parseToken
+  // we can only initialize the pool with base asset, the yield asset is ignored.
+  const maxAmountsIn = [parseEther(amountIn), parseEther(amountIn)];
+  const amounts = maxAmountsIn.map((amt) => amt.toHexString());
+
+  // Whether or not to use balances held in balancer.  Since The Vault has nothing, set this to false.
+  const fromInternalBalance = false;
+
+  // Allow balancer pool to take user's fyt and base tokens
+  await baseAssetContract.approve(vaultContract.address, MAX_ALLOWANCE);
+  await trancheContract.approve(vaultContract.address, MAX_ALLOWANCE);
+
+  // Balancer V2 vault allows userData as a way to pass props through to pool contracts.  In our
+  // case we need to pass the maxAmountsIn.
+
+  const userData = ethers.utils.defaultAbiCoder.encode(["uint256[]"], amounts);
+
+  const joinReceipt = await vaultContract.joinPool(
+    poolId,
+    elementAddress,
+    elementAddress,
+    tokens,
+    maxAmountsIn,
+    fromInternalBalance,
+    userData
+  );
+
+  await joinReceipt.wait(1);
+  return joinReceipt;
+}

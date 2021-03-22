e0c1447217c5dfab8ad5dfe666809a9126cdb2b3
Matthew Brown <mattfdbrown@gmail.com>
2021-03-22 13:23:59 -0700

    fix tests

--- test/helpers/deployBalancerVault.ts
+++ test/helpers/deployBalancerVault.ts
@@ -4,10 +4,15 @@ import { Signer } from "ethers";
 
 import { Vault__factory } from "typechain/factories/Vault__factory";
 
-export async function deployBalancerVault(signer: Signer) {
+export async function deployBalancerVault(signer: Signer, wethAddress: string) {
   const signerAddress = await signer.getAddress();
   const vaultDeployer = new Vault__factory(signer);
-  const vaultContract = await vaultDeployer.deploy(signerAddress, 0, 0);
+  const vaultContract = await vaultDeployer.deploy(
+    signerAddress,
+    wethAddress,
+    0,
+    0
+  );
 
   await vaultContract.deployed();
 

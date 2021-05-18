4e60334009df330791958fb7dc88f96e0b23c2d0
NicholasDotSol <nicholas@element.fi>
2021-05-18 09:44:37 -0500

    Yearn Shares Zapper (#194)
    * Implement share zapper
    
    The share zapper takes yearn LPs as an imput and
    mints Yield and Principal Tokens.
    
    * Test share zapper
    
    * remove .only from zapYearnShares test
    
    * fix file name typo
    
    * Remove loss parameter
    
    If there is loss we do not allow entry
    
    * avoind unnecessary Transfer
    
    Mint directly to the position address
    
    * add expected PT received on zapper
    
    * Updates tests
    
    * add _ptExpected natspec
    
    Co-authored-by: Nicholas Evans <nicholas.evans@thesis.co>

---
+++ contracts/zaps/ZapYearnShares.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "../interfaces/IYearnVaultV2.sol";
+import "../libraries/Authorizable.sol";
+import "../interfaces/IERC20.sol";
+import "../interfaces/ITranche.sol";
+
+contract ZapYearnShares is Authorizable {
+    // Store the accessibility state of the contract
+    bool public isFrozen = false;
+    // Tranche factory address for Tranche contract address derivation
+    address internal immutable _trancheFactory;
+    // Tranche bytecode hash for Tranche contract address derivation.
+    // This is constant as long as Tranche does not implement non-constant constructor arguments.
+    bytes32 internal immutable _trancheBytecodeHash;
+
+    /// @param __trancheFactory Address of the TrancheFactory contract
+    /// @param __trancheBytecodeHash Hash of the Tranche bytecode.
+    constructor(address __trancheFactory, bytes32 __trancheBytecodeHash)
+        Authorizable()
+    {
+        _authorize(msg.sender);
+        _trancheFactory = __trancheFactory;
+        _trancheBytecodeHash = __trancheBytecodeHash;
+    }
+
+    /// @dev Requires that the contract is not frozen
+    modifier notFrozen() {
+        require(!isFrozen, "Contract frozen");
+        _;
+    }
+
+    /// @dev Allows an authorized address to freeze or unfreeze this contract
+    /// @param _newState True for frozen and false for unfrozen
+    function setIsFrozen(bool _newState) external onlyAuthorized() {
+        isFrozen = _newState;
+    }
+
+    /// @notice Mints a Principal/Interest token pair from yearn vault shares.
+    ///      then returns the tokens to the caller.
+    /// @param _underlying The underlying ERC20 token contract of the yearn vault.
+    /// @param _vault The address of the target yearn vault.
+    /// @param _amount The amount of yearn shares to turn into tokens
+    /// @param _expiration The expiration time of the Tranche contract.
+    /// @param _position The contract which manages pooled deposits.
+    /// @param _ptExpected The minimum amount of principal tokens to mint.
+    /// @return returns the minted amounts of principal and yield tokens (PT and YT)
+    function zapSharesIn(
+        IERC20 _underlying,
+        IYearnVault _vault,
+        uint256 _amount,
+        uint256 _expiration,
+        address _position,
+        uint256 _ptExpected
+    ) external notFrozen() returns (uint256, uint256) {
+        _vault.transferFrom(msg.sender, address(this), _amount);
+        _vault.withdraw(_amount, _position, 0);
+
+        ITranche tranche = _deriveTranche(address(_position), _expiration);
+        uint256 balance = _underlying.balanceOf(_position);
+
+        (uint256 ptMinted, uint256 ytMinted) = tranche.prefundedDeposit(
+            msg.sender
+        );
+        require(ytMinted >= balance, "Not enough YT minted");
+        require(ptMinted >= _ptExpected, "Not enough PT minted");
+        return (ptMinted, ytMinted);
+    }
+
+    /// @dev This internal function produces the deterministic create2
+    ///      address of the Tranche contract from a wrapped position contract and expiration
+    /// @param _position The wrapped position contract address
+    /// @param _expiration The expiration time of the tranche
+    /// @return The derived Tranche contract
+    function _deriveTranche(address _position, uint256 _expiration)
+        internal
+        virtual
+        view
+        returns (ITranche)
+    {
+        bytes32 salt = keccak256(abi.encodePacked(_position, _expiration));
+        bytes32 addressBytes = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                _trancheFactory,
+                salt,
+                _trancheBytecodeHash
+            )
+        );
+        return ITranche(address(uint160(uint256(addressBytes))));
+    }
+
+    /// @dev This contract can hold yearn vault share allowances for addresses so if it is deprecated
+    ///      it should be removed so that users do not have to remove allowances.
+    ///      Note - onlyOwner is a stronger check than onlyAuthorized, many addresses can be
+    ///      authorized to freeze or unfreeze the contract but only the owner address can kill
+    function deprecate() external onlyOwner() {
+        selfdestruct(payable(msg.sender));
+    }
+}
---
+++ test/zapYearnSharesTest.ts
@@ -0,0 +1,127 @@
+import { expect } from "chai";
+import { Signer, BigNumber } from "ethers";
+import { ethers, waffle } from "hardhat";
+import {
+  loadYearnShareZapFixture,
+  YearnShareZapInterface,
+} from "./helpers/deployer";
+import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
+import { advanceTime, getCurrentTimestamp } from "./helpers/time";
+import { impersonate, stopImpersonating } from "./helpers/impersonate";
+import { subError, bnFloatMultiplier } from "./helpers/math";
+import { ZapYearnShares__factory } from "../typechain/factories/ZapYearnShares__factory";
+import { ZapYearnShares } from "../typechain/ZapYearnShares";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import data from "../artifacts/contracts/Tranche.sol/Tranche.json";
+
+const { provider } = waffle;
+
+describe("zapYearnShares", () => {
+  let users: { user: Signer; address: string }[];
+  let fixture: YearnShareZapInterface;
+
+  before(async () => {
+    // snapshot initial state
+    await createSnapshot(provider);
+    fixture = await loadYearnShareZapFixture();
+
+    // begin to populate the user array by assigning each index a signer
+    users = ((await ethers.getSigners()) as Signer[]).map(function (user) {
+      return { user, address: "" };
+    });
+
+    // finish populating the user array by assigning each index a signer address
+    await Promise.all(
+      users.map(async (userInfo) => {
+        const { user } = userInfo;
+        userInfo.address = await user.getAddress();
+      })
+    );
+    // get USDC
+    const usdcWhaleAddress = "0xAe2D4617c862309A3d75A0fFB358c7a5009c673F";
+    impersonate(usdcWhaleAddress);
+    const usdcWhale = await ethers.provider.getSigner(usdcWhaleAddress);
+    await fixture.usdc.connect(usdcWhale).transfer(users[1].address, 2e11); // 200k usdc
+    stopImpersonating(usdcWhaleAddress);
+  });
+  after(async () => {
+    // revert back to initial state after all tests pass
+    await restoreSnapshot(provider);
+  });
+  beforeEach(async () => {
+    await createSnapshot(provider);
+  });
+  afterEach(async () => {
+    await restoreSnapshot(provider);
+  });
+
+  describe("zapSharesIn", () => {
+    beforeEach(async () => {
+      await createSnapshot(provider);
+    });
+
+    afterEach(async () => {
+      await restoreSnapshot(provider);
+    });
+    it("should fail with incorrect PT expected", async () => {
+      // deposit value directly into the yearn vault
+      await fixture.usdc
+        .connect(users[1].user)
+        .approve(fixture.yusdc.address, 2e11);
+      await fixture.yusdc
+        .connect(users[1].user)
+        .deposit(2e11, users[1].address);
+      const shares = await fixture.yusdc.balanceOf(users[1].address);
+
+      // zap shares into PT and YT
+      const expiration = (await fixture.tranche.unlockTimestamp()).toNumber();
+      await fixture.yusdc
+        .connect(users[1].user)
+        .approve(fixture.sharesZapper.address, shares);
+      const tx = fixture.sharesZapper
+        .connect(users[1].user)
+        .zapSharesIn(
+          fixture.usdc.address,
+          fixture.yusdc.address,
+          shares,
+          expiration,
+          fixture.position.address,
+          shares.mul(2)
+        );
+      await expect(tx).to.be.revertedWith("Not enough PT minted");
+    });
+    it("should correctly zap shares in", async () => {
+      // deposit value directly into the yearn vault
+      await fixture.usdc
+        .connect(users[1].user)
+        .approve(fixture.yusdc.address, 2e11);
+      await fixture.yusdc
+        .connect(users[1].user)
+        .deposit(2e11, users[1].address);
+      const shares = await fixture.yusdc.balanceOf(users[1].address);
+      // zap shares into PT and YT
+      const expiration = (await fixture.tranche.unlockTimestamp()).toNumber();
+      await fixture.yusdc
+        .connect(users[1].user)
+        .approve(fixture.sharesZapper.address, shares);
+      await fixture.sharesZapper
+        .connect(users[1].user)
+        .zapSharesIn(
+          fixture.usdc.address,
+          fixture.yusdc.address,
+          shares,
+          expiration,
+          fixture.position.address,
+          shares
+        );
+      const pricePerFullShare = await fixture.yusdc.pricePerShare();
+      const balance = (
+        await (await fixture.yusdc.balanceOf(fixture.position.address)).mul(
+          pricePerFullShare
+        )
+      ).div(ethers.utils.parseUnits("1", 6));
+      // Allows a 0.01% conversion error
+      expect(balance).to.be.at.least(subError(ethers.BigNumber.from(1e11)));
+    });
+  });
+});

4be95d9ee900c021ed5084d97e83190795bd25a5
Paul Vienhage <paul@0x.org>
2021-09-17 16:56:14 +0100

    remove gas reserve

--- contracts/YVaultAssetProxy.sol
+++ contracts/YVaultAssetProxy.sol
@@ -10,20 +10,9 @@ import "./WrappedPosition.sol";
 /// @author Element Finance
 /// @title Yearn Vault v1 Asset Proxy
 contract YVaultAssetProxy is WrappedPosition {
+    // the yearn vault and how many decimals it's deposit receipt tokens are
     IYearnVault public immutable vault;
     uint8 public immutable vaultDecimals;
-
-    // This contract allows deposits to a reserve which can
-    // be used to short circuit the deposit process and save gas
-
-    // The following mapping tracks those non-transferable deposits
-    mapping(address => uint256) public reserveBalances;
-    // These variables store the token balances of this contract and
-    // should be packed by solidity into a single slot.
-    uint128 public reserveUnderlying;
-    uint128 public reserveShares;
-    // This is the total amount of reserve deposits
-    uint256 public reserveSupply;
 
     /// @notice Constructs this contract and stores needed data
     /// @param vault_ The yearn v2 vault
@@ -45,187 +34,38 @@ contract YVaultAssetProxy is WrappedPosition {
             uint8(_token.decimals()) == localVaultDecimals,
             "Inconsistent decimals"
         );
-        // We check that this is a compatible yearn version
-        _versionCheck(IYearnVault(vault_));
-    }
-
-    /// @notice An override-able version checking function, reverts if the vault has the wrong version
-    /// @param _vault The yearn vault address
-    /// @dev This function can be overridden by an inheriting upgrade contract
-    function _versionCheck(IYearnVault _vault) internal virtual view {
-        string memory apiVersion = _vault.apiVersion();
-        require(
-            _stringEq(apiVersion, "0.3.0") ||
-                _stringEq(apiVersion, "0.3.1") ||
-                _stringEq(apiVersion, "0.3.2") ||
-                _stringEq(apiVersion, "0.3.3") ||
-                _stringEq(apiVersion, "0.3.4") ||
-                _stringEq(apiVersion, "0.3.5"),
-            "Unsupported Version"
-        );
-    }
-
-    /// @notice checks if two strings are equal
-    /// @param s1 string one
-    /// @param s2 string two
-    /// @return bool whether they are equal
-    function _stringEq(string memory s1, string memory s2)
-        internal
-        pure
-        returns (bool)
-    {
-        bytes32 h1 = keccak256(abi.encodePacked(s1));
-        bytes32 h2 = keccak256(abi.encodePacked(s2));
-        return (h1 == h2);
-    }
-
-    /// @notice This function allows a user to deposit to the reserve
-    ///      Note - there's no incentive to do so. You could earn some
-    ///      interest but less interest than yearn. All deposits use
-    ///      the underlying token.
-    /// @param _amount The amount of underlying to deposit
-    function reserveDeposit(uint256 _amount) external {
-        // Transfer from user, note variable 'token' is the immutable
-        // inherited from the abstract WrappedPosition contract.
-        token.transferFrom(msg.sender, address(this), _amount);
-        // Load the reserves
-        (uint256 localUnderlying, uint256 localShares) = _getReserves();
-        // Calculate the total reserve value
-        uint256 totalValue = localUnderlying;
-        totalValue += _yearnDepositConverter(localShares, true);
-        // If this is the first deposit we need different logic
-        uint256 localReserveSupply = reserveSupply;
-        uint256 mintAmount;
-        if (localReserveSupply == 0) {
-            // If this is the first mint the tokens are exactly the supplied underlying
-            mintAmount = _amount;
-        } else {
-            // Otherwise we mint the proportion that this increases the value held by this contract
-            mintAmount = (localReserveSupply * _amount) / totalValue;
-        }
-
-        // This hack means that the contract will never have zero balance of underlying
-        // which levels the gas expenditure of the transfer to this contract. Permanently locks
-        // the smallest possible unit of the underlying.
-        if (localUnderlying == 0 && localShares == 0) {
-            _amount -= 1;
-        }
-        // Set the reserves that this contract has more underlying
-        _setReserves(localUnderlying + _amount, localShares);
-        // Note that the sender has deposited and increase reserveSupply
-        reserveBalances[msg.sender] += mintAmount;
-        reserveSupply = localReserveSupply + mintAmount;
-    }
-
-    /// @notice This function allows a holder of reserve balance to withdraw their share
-    /// @param _amount The number of reserve shares to withdraw
-    function reserveWithdraw(uint256 _amount) external {
-        // Remove 'amount' from the balances of the sender. Because this is 8.0 it will revert on underflow
-        reserveBalances[msg.sender] -= _amount;
-        // We load the reserves
-        (uint256 localUnderlying, uint256 localShares) = _getReserves();
-        uint256 localReserveSupply = reserveSupply;
-        // Then we calculate the proportion of the shares to redeem
-        uint256 userShares = (localShares * _amount) / localReserveSupply;
-        // First we withdraw the proportion of shares tokens belonging to the caller
-        uint256 freedUnderlying = vault.withdraw(userShares, address(this), 0);
-        // We calculate the amount of underlying to send
-        uint256 userUnderlying = (localUnderlying * _amount) /
-            localReserveSupply;
-
-        // We then store the updated reserve amounts
-        _setReserves(
-            localUnderlying - userUnderlying,
-            localShares - userShares
-        );
-        // We note a reduction in local supply
-        reserveSupply = localReserveSupply - _amount;
-
-        // We send the redemption underlying to the caller
-        // Note 'token' is an immutable from shares
-        token.transfer(msg.sender, freedUnderlying + userUnderlying);
     }
 
     /// @notice Makes the actual deposit into the yearn vault
-    ///         Tries to use the local balances before depositing
     /// @return Tuple (the shares minted, amount underlying used)
     function _deposit() internal override returns (uint256, uint256) {
-        //Load reserves
-        (uint256 localUnderlying, uint256 localShares) = _getReserves();
         // Get the amount deposited
-        uint256 amount = token.balanceOf(address(this)) - localUnderlying;
-        // fixing for the fact there's an extra underlying
-        if (localUnderlying != 0 || localShares != 0) {
-            amount -= 1;
-        }
-        // Calculate the amount of shares the amount deposited is worth
-        uint256 neededShares = _yearnDepositConverter(amount, false);
+        uint256 amount = token.balanceOf(address(this));
 
-        // If we have enough in local reserves we don't call out for deposits
-        if (localShares > neededShares) {
-            // We set the reserves
-            _setReserves(localUnderlying + amount, localShares - neededShares);
-            // And then we short circuit execution and return
-            return (neededShares, amount);
-        }
         // Deposit and get the shares that were minted to this
-        uint256 shares = vault.deposit(localUnderlying + amount, address(this));
+        uint256 shares = vault.deposit(amount, address(this));
 
-        // calculate the user share
-        uint256 userShare = (amount * shares) / (localUnderlying + amount);
-
-        // We set the reserves
-        _setReserves(0, localShares + shares - userShare);
         // Return the amount of shares the user has produced, and the amount used for it.
-        return (userShare, amount);
+        return (shares, amount);
     }
 
-    /// @notice Withdraw the number of shares and will short circuit if it can
+    /// @notice Withdraw the number of shares
     /// @param _shares The number of shares to withdraw
     /// @param _destination The address to send the output funds
-    /// @param _underlyingPerShare The possibly precomputed underlying per share
+    // @param _underlyingPerShare The possibly precomputed underlying per share
+    /// @return returns the amount of funds freed by doing a yearn withdraw
     function _withdraw(
         uint256 _shares,
         address _destination,
-        uint256 _underlyingPerShare
+        uint256
     ) internal override returns (uint256) {
-        // If we do not have it we load the price per share
-        if (_underlyingPerShare == 0) {
-            _underlyingPerShare = _pricePerShare();
-        }
-        // We load the reserves
-        (uint256 localUnderlying, uint256 localShares) = _getReserves();
-        // Calculate the amount of shares the amount deposited is worth
-        uint256 needed = (_shares * _pricePerShare()) / (10**vaultDecimals);
-        // If we have enough underlying we don't have to actually withdraw
-        if (needed < localUnderlying) {
-            // We set the reserves to be the new reserves
-            _setReserves(localUnderlying - needed, localShares + _shares);
-            // Then transfer needed underlying to the destination
-            // 'token' is an immutable in WrappedPosition
-            token.transfer(_destination, needed);
-            // Short circuit and return
-            return (needed);
-        }
-        // If we don't have enough local reserves we do the actual withdraw
         // Withdraws shares from the vault. Max loss is set at 100% as
         // the minimum output value is enforced by the calling
         // function in the WrappedPosition contract.
-        uint256 amountReceived = vault.withdraw(
-            _shares + localShares,
-            address(this),
-            10000
-        );
+        uint256 amountReceived = vault.withdraw(_shares, _destination, 10000);
 
-        // calculate the user share
-        uint256 userShare = (_shares * amountReceived) /
-            (localShares + _shares);
-
-        _setReserves(localUnderlying + amountReceived - userShare, 0);
-        // Transfer the underlying to the destination 'token' is an immutable in WrappedPosition
-        token.transfer(_destination, userShare);
         // Return the amount of underlying
-        return userShare;
+        return amountReceived;
     }
 
     /// @notice Get the underlying amount of tokens per shares given
@@ -250,51 +90,5 @@ contract YVaultAssetProxy is WrappedPosition {
     function approve() external {
         token.approve(address(vault), 0);
         token.approve(address(vault), type(uint256).max);
-    }
-
-    /// @notice Helper to get the reserves with one sload
-    /// @return Tuple (reserve underlying, reserve shares)
-    function _getReserves() internal view returns (uint256, uint256) {
-        return (uint256(reserveUnderlying), uint256(reserveShares));
-    }
-
-    /// @notice Helper to set reserves using one sstore
-    /// @param _newReserveUnderlying The new reserve of underlying
-    /// @param _newReserveShares The new reserve of wrapped position shares
-    function _setReserves(
-        uint256 _newReserveUnderlying,
-        uint256 _newReserveShares
-    ) internal {
-        reserveUnderlying = uint128(_newReserveUnderlying);
-        reserveShares = uint128(_newReserveShares);
-    }
-
-    /// @notice Converts an input of shares to it's output of underlying or an input
-    ///      of underlying to an output of shares, using yearn 's deposit pricing
-    /// @param amount the amount of input, shares if 'sharesIn == true' underlying if not
-    /// @param sharesIn true to convert from yearn shares to underlying, false to convert from
-    ///                 underlying to yearn shares
-    /// @dev WARNING - In yearn 0.3.1 - 0.3.5 this is an exact match for deposit logic
-    ///                but not withdraw logic in versions 0.3.2-0.3.5. In versions 0.4.0+
-    ///                it is not a match for yearn deposit ratios.
-    /// @return The converted output of either underlying or yearn shares
-    function _yearnDepositConverter(uint256 amount, bool sharesIn)
-        internal
-        virtual
-        view
-        returns (uint256)
-    {
-        // Load the yearn total supply and assets
-        uint256 yearnTotalSupply = vault.totalSupply();
-        uint256 yearnTotalAssets = vault.totalAssets();
-        // If we are converted shares to underlying
-        if (sharesIn) {
-            // then we get the fraction of yearn shares this is and multiply by assets
-            return (yearnTotalAssets * amount) / yearnTotalSupply;
-        } else {
-            // otherwise we figure out the faction of yearn assets this is and see how
-            // many assets we get out.
-            return (yearnTotalSupply * amount) / yearnTotalAssets;
-        }
     }
 }
--- test/wrappedPositionTest.ts
+++ test/wrappedPositionTest.ts
@@ -5,9 +5,6 @@ import { ethers, waffle } from "hardhat";
 import { FixtureInterface, loadFixture } from "./helpers/deployer";
 import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
 
-import { TestYVaultV4 } from "typechain/TestYVaultV4";
-import { YVaultV4AssetProxy } from "typechain/YVaultV4AssetProxy";
-
 const { provider } = waffle;
 
 describe("Wrapped Position", () => {
@@ -47,29 +44,6 @@ describe("Wrapped Position", () => {
   after(async () => {
     // revert back to initial state after all tests pass
     await restoreSnapshot(provider);
-  });
-  describe("Version locked deployments", () => {
-    it("Wont deploy YAssetProxy with a v4 yearn vault", async () => {
-      // Deploy a mock yearn v4
-      const mockYearnV4Factory = await ethers.getContractFactory(
-        "TestYVaultV4"
-      );
-      const yVaultV4 = await mockYearnV4Factory.deploy(
-        fixture.usdc.address,
-        await fixture.usdc.decimals()
-      );
-      // Attempt to deploy a non v4 wp on it
-      const yearnWPFactory = await ethers.getContractFactory(
-        "YVaultAssetProxy"
-      );
-      const tx = yearnWPFactory.deploy(
-        yVaultV4.address,
-        fixture.usdc.address,
-        "mock yearn",
-        "my"
-      );
-      await expect(tx).to.be.revertedWith("Unsupported Version");
-    });
   });
   describe("balanceOfUnderlying", () => {
     beforeEach(async () => {
@@ -174,159 +148,6 @@ describe("Wrapped Position", () => {
         .add(usdcBalanceUser1)
         .add(usdcBalanceUser2);
       expect(totalUsdcBalance).to.equal(ethers.BigNumber.from("19000000"));
-    });
-  });
-  // These tests are of the reserve mechanic
-  describe("Reserve deposit and withdraw", async () => {
-    beforeEach(async () => {
-      await createSnapshot(provider);
-    });
-    afterEach(async () => {
-      await restoreSnapshot(provider);
-    });
-    it("Successfully deposits", async () => {
-      // Create some underlying
-      await fixture.usdc.mint(users[0].address, 100e6);
-      // Approve the wrapped position reserve
-      await fixture.usdc.approve(fixture.position.address, 100e6);
-      // deposit to the reserves
-      await fixture.position.reserveDeposit(10e6);
-      expect(await fixture.position.reserveUnderlying()).to.be.eq(10e6 - 1);
-      expect(await fixture.position.reserveShares()).to.be.eq(0);
-      expect(await fixture.position.reserveBalances(users[0].address)).to.be.eq(
-        10e6
-      );
-      expect(await fixture.position.reserveSupply()).to.be.eq(10e6);
-      // We make a small deposit to change the reserve
-      await fixture.position
-        .connect(users[1].user)
-        .deposit(users[1].address, 1e6);
-      // NOTE - THIS CONSTANT DEPENDS ON THE PREVIOUS TEST SETTING THE DEPOSIT RATE
-      expect(await fixture.position.balanceOf(users[1].address)).to.equal(
-        909090
-      );
-      // We then make a new reserve deposit
-      await fixture.position.connect(users[2].user).reserveDeposit(1e6);
-      expect(await fixture.position.reserveUnderlying()).to.be.eq(1e6);
-      expect(await fixture.position.reserveShares()).to.be.eq(9090909);
-      expect(await fixture.position.reserveBalances(users[2].address)).to.be.eq(
-        1e6
-      );
-      expect(await fixture.position.reserveSupply()).to.be.eq(11e6);
-    });
-
-    it("Successfully withdraws", async () => {
-      // Create some underlying
-      await fixture.usdc.mint(users[0].address, 10e6);
-      // Approve the position reserve
-      await fixture.usdc.approve(fixture.position.address, 100e6);
-      // deposit to the reserve
-      await fixture.position.reserveDeposit(10e6);
-      const balanceStart = await fixture.usdc.balanceOf(users[0].address);
-      // We do a trial withdraw
-      await fixture.position.reserveWithdraw(1e6);
-      let currentBalance = await fixture.usdc.balanceOf(users[0].address);
-      // Note - this is slightly less because underlying reserves are missing
-      //        exactly one unit of usdc
-      expect(currentBalance).to.be.eq(balanceStart.add(999999));
-      // We now check that reserve withdraws with shares present work properly
-      // Make a trade which requires local shares
-      await fixture.position
-        .connect(users[1].user)
-        .deposit(users[1].address, 1e6);
-      expect(await fixture.position.reserveShares()).to.be.eq(8181819);
-      // We now make a withdraw and check that the right amount is produced
-      await fixture.position.reserveWithdraw(1e6);
-      currentBalance = await fixture.usdc.balanceOf(users[0].address);
-      expect(currentBalance).to.be.eq(balanceStart.add(999999).add(1e6));
-      expect(await fixture.position.reserveShares()).to.be.eq(7272728);
-    });
-  });
-  // We test explicity only the functionality changes in V4 not the whole
-  // contract
-  describe("Wrapped Yearn Vault v4 upgrade", async () => {
-    let vaultV4: TestYVaultV4;
-    let wpV4: YVaultV4AssetProxy;
-
-    before(async () => {
-      // Deploy a mock yearn v4
-      const mockYearnV4Factory = await ethers.getContractFactory(
-        "TestYVaultV4"
-      );
-      // TODO - Update so that the ethers factories play nice with types
-      vaultV4 = (await mockYearnV4Factory.deploy(
-        fixture.usdc.address,
-        await fixture.usdc.decimals()
-      )) as TestYVaultV4;
-      // Deploy a wrapped position
-      const wpFactory = await ethers.getContractFactory("YVaultV4AssetProxy");
-      wpV4 = (await wpFactory.deploy(
-        vaultV4.address,
-        fixture.usdc.address,
-        "mock yearn v4",
-        "test"
-      )) as YVaultV4AssetProxy;
-      // set approvals for accounts
-      await fixture.usdc.approve(wpV4.address, ethers.constants.MaxUint256);
-      await fixture.usdc
-        .connect(users[1].user)
-        .approve(wpV4.address, ethers.constants.MaxUint256);
-      // deposit into the yearn vault so there's a supply
-      await fixture.usdc.approve(vaultV4.address, ethers.constants.MaxUint256);
-      await vaultV4.deposit(1e6, users[0].address);
-    });
-
-    beforeEach(async () => {
-      await createSnapshot(provider);
-    });
-    afterEach(async () => {
-      // revert back to initial state after all tests pass
-      await restoreSnapshot(provider);
-    });
-
-    it("Won't deploy on a v3 wrapped position", async () => {
-      const wpFactory = await ethers.getContractFactory("YVaultAssetProxy");
-      const tx = wpFactory.deploy(
-        vaultV4.address,
-        fixture.usdc.address,
-        "mock yearn v4",
-        "test"
-      );
-      await expect(tx).to.be.revertedWith("Unsupported Version");
-    });
-
-    it("Reserve deposits", async () => {
-      // First deposit inits
-      await wpV4.reserveDeposit(1e6);
-      // Check that we got shares
-      let balance = await wpV4.reserveBalances(users[0].address);
-      expect(balance).to.be.eq(BigNumber.from(1e6));
-      // Do a deposit to convert to shares
-      await wpV4.connect(users[1].user).deposit(users[1].address, 1e6);
-      console.log(await fixture.usdc.balanceOf(vaultV4.address));
-      // We want to change deposit rate
-      await fixture.usdc.transfer(vaultV4.address, 5e5);
-      // Now we deposit and check the price of deposit
-      await wpV4.reserveDeposit(1e6);
-      // Check the new balance
-      balance = await wpV4.reserveBalances(users[0].address);
-      // Magic number from rounding error on deposits
-      expect(balance).to.be.eq(BigNumber.from(1857144));
-    });
-
-    it("withdraws with the correct ratio", async () => {
-      await wpV4.deposit(users[0].address, 1e6);
-      // Check that we got shares
-      const balance = await wpV4.balanceOf(users[0].address);
-      expect(balance).to.be.eq(BigNumber.from(1e6));
-      // We want to change withdraw rate
-      await fixture.usdc.transfer(vaultV4.address, 5e5);
-      // withdraw
-      const balanceBefore = await fixture.usdc.balanceOf(users[0].address);
-      await wpV4.withdraw(users[0].address, balance, 0);
-      const balanceAfter = await fixture.usdc.balanceOf(users[0].address);
-      // check the withdraw amount
-      expect(balanceAfter.sub(balanceBefore)).to.be.eq(125e4);
     });
   });
 });

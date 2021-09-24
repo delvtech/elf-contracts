75365debd6292e683f4c3764e76a53ba2851852d
Sarah Mackey <sarahmackey42@gmail.com>
2021-09-23 22:07:15 -0700

    add interface

--- contracts/CompoundAssetProxy.sol
+++ contracts/CompoundAssetProxy.sol
@@ -3,87 +3,51 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/IERC20.sol";
+import "./interfaces/ICompoundVault.sol";
 import "./WrappedPosition.sol";
 
 /// @author Element Finance
 /// @title Compound Asset Proxy
 contract CompoundAssetProxy is WrappedPosition {
-    // vault;
-    uint8 public immutable vaultDecimals;
+    ICompoundVault public immutable vault;
 
-    // The following mapping tracks those non-transferable deposits
+    // decimals stuff is confusing to me
+    uint8 public immutable vaultDecimals;
     mapping(address => uint256) public reserveBalances;
-    // These variables store the token balances of this contract and
-    // should be packed by solidity into a single slot.
+
+    // it's interesting that this is uint128 whereas the input to the setter function is uint128
     uint128 public reserveUnderlying;
     uint128 public reserveShares;
-    // This is the total amount of reserve deposits
-    uint256 public reserveSupply;
 
-    /// @notice Constructs this contract and stores needed data
-    /// @param vault_ The compound vault
-    /// @param _token The underlying token.
-    ///               This token should revert in the event of a transfer failure.
-    /// @param _name The name of the token created
-    /// @param _symbol The symbol of the token created
+    // uint256 public reserveSupply; don't understand exactly what this does
+
     constructor(
         address vault_,
         IERC20 _token,
         string memory _name,
         string memory _symbol
-    ) WrappedPosition(_token, _name, _symbol) {}
+    ) {
+        // the Yearn asset proxy does interesting things here with WrappedPosition
 
-    /// @notice This function allows a user to deposit to the reserve
-    /// @param _amount The amount of underlying to deposit
-    function reserveDeposit(uint256 _amount) external {}
+        // I'm not confident this is how the vault is going to work
+        vault = ICompoundVault(vault_);
 
-    /// @notice This function allows a holder of reserve balance to withdraw their share
-    /// @param _amount The number of reserve shares to withdraw
-    function reserveWithdraw(uint256 _amount) external {}
+        // not sure what this approve step does, why we're using max
+        _token.approve(vault_, type(uint256).max);
 
-    function priceCheck() external {}
-
-    /// @notice Makes the actual deposit into the yearn vault
-    ///         Tries to use the local balances before depositing
-    /// @return Tuple (the shares minted, amount underlying used)
-    function _deposit() internal override returns (uint256, uint256) {
-        //Load reserves
-        (uint256 localUnderlying, uint256 localShares) = _getReserves();
-        // Get the amount deposited
-        uint256 amount = token.balanceOf(address(this)) - localUnderlying;
-        // fixing for the fact there's an extra underlying
-        if (localUnderlying != 0 || localShares != 0) {
-            amount -= 1;
-        }
-        // Calculate the amount of shares the amount deposited is worth
-
-        // If we have enough in local reserves we don't call out for deposits
-        if (localShares > neededShares) {
-            // We set the reserves
-            _setReserves(localUnderlying + amount, localShares - neededShares);
-            // And then we short circuit execution and return
-            return (neededShares, amount);
-        }
-        // Deposit and get the shares that were minted to this
-        uint256 shares = vault.deposit(localUnderlying + amount, address(this));
-
-        // calculate the user share
-        uint256 userShare = (amount * shares) / (localUnderlying + amount);
-
-        // We set the reserves
-        _setReserves(0, localShares + shares - userShare);
-        // Return the amount of shares the user has produced, and the amount used for it.
+        // I'm not entirely sure if this should be IERC20 from our interfaces or some
+        // implementation of Compound's CErc20
+        vaultDecimals = IERC20(vault_).decimals();
+        require(
+            uint8(_token.decimals()) == vaultDecimals,
+            "Inconsistent decimals"
+        );
     }
 
-    /// @notice Helper to get the reserves with one sload
-    /// @return Tuple (reserve underlying, reserve shares)
     function _getReserves() internal view returns (uint256, uint256) {
         return (uint256(reserveUnderlying), uint256(reserveShares));
     }
 
-    /// @notice Helper to set reserves using one sstore
-    /// @param _newReserveUnderlying The new reserve of underlying
-    /// @param _newReserveShares The new reserve of wrapped position shares
     function _setReserves(
         uint256 _newReserveUnderlying,
         uint256 _newReserveShares
@@ -92,13 +56,89 @@ contract CompoundAssetProxy is WrappedPosition {
         reserveShares = uint128(_newReserveShares);
     }
 
-    /// @notice Withdraw the number of shares and will short circuit if it can
-    /// @param _shares The number of shares to withdraw
-    /// @param _destination The address to send the output funds
-    /// @param _underlyingPerShare The possibly precomputed underlying per share
+    function _deposit() internal override returns (uint256, uint256) {
+        // Load reserves
+        (uint256 localUnderlying, uint256 localShares) = _getReserves();
+        //Get the amount deposited
+        uint256 amount = token.balanceOf(address(this)) - localUnderlying;
+
+        // Calculate the amount of shares the amount deposited is worth
+        uint256 neededShares = _compoundDepositConverter(amount, false);
+
+        // Deposit and get the shares that were minted to this
+        uint256 shares = vault.deposit(localUnderlying + amount, address(this));
+
+        // calculate the user share
+        uint256 userShare = (amount * shares) / (localUnderlying + amount);
+
+        // set the reserves
+        _setReserves(0, localShares + shares - userShare);
+        // Return the amount of shares the user has produced, and the amount used for it.
+        return (userShare, amount);
+    }
+
     function _withdraw(
         uint256 _shares,
         address _destination,
         uint256 _underlyingPerShare
-    ) internal override returns (uint256) {}
+    ) internal override returns (uint256) {
+        // load the reserves
+        (uint256 localUnderlying, uint256 localShares) = _getReserves();
+
+        // do the withdraw
+        uint256 amountReceived = vault.withdraw(
+            _shares + localShares,
+            address(this),
+            10000
+        );
+
+        // calculate the user share
+        uint256 userShare = (_shares * amountReceived) /
+            (localShares + _shares);
+
+        _setReserves(localUnderlying + amountReceived - userShare, 0);
+        // Transfer the underlying to the destination 'token' is an immutable in WrappedPosition
+        token.transfer(_destination, userShare);
+        // Return the amount of underlying
+        return userShare;
+    }
+
+    /// @notice Get the underlying amount of tokens per shares given
+    /// @param _amount The amount of shares you want to know the value of
+    /// @return Value of shares in underlying token
+    function _underlying(uint256 _amount)
+        internal
+        override
+        view
+        returns (uint256)
+    {
+        return (_amount * _pricePerShare()) / (10**vaultDecimals);
+    }
+
+    /// @notice Get the price per share in the vault
+    /// @return The price per share in units of underlying;
+    function _pricePerShare() internal view returns (uint256) {
+        return vault.pricePerShare();
+    }
+
+    // not sure if we need a compound version of this or not
+    function _compoundDepositConverter(uint256 amount, bool sharesIn)
+        internal
+        virtual
+        view
+        returns (uint256)
+    {
+        // Load the yearn total supply and assets
+        uint256 compoundTotalSupply = vault.totalSupply();
+        uint256 compoundTotalAssets = vault.totalAssets();
+        // If we are converted shares to underlying
+        if (sharesIn) {
+            // then we get the fraction of yearn shares this is and multiply by assets
+            return (compoundTotalAssets * amount) / compoundTotalSupply;
+        } else {
+            // otherwise we figure out the faction of yearn assets this is and see how
+            // many assets we get out.
+            return (compoundTotalSupply * amount) / compoundTotalAssets;
+        }
+    }
 }
---
+++ contracts/interfaces/ICompoundVault.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol";
+
+interface ICompoundVault is IERC20 {
+    function deposit(uint256, address) external returns (uint256);
+
+    function withdraw(
+        uint256,
+        address,
+        uint256
+    ) external returns (uint256);
+
+    // Returns the amount of underlying per each unit [1e18] of yearn shares
+    function pricePerShare() external view returns (uint256);
+
+    function governance() external view returns (address);
+
+    function setDepositLimit(uint256) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function totalAssets() external view returns (uint256);
+}

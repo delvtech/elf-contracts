ecac13d8068752b37d1b3dd7022036ab1906ba62
Sarah Mackey <sarahmackey42@gmail.com>
2021-11-02 15:37:28 -0700

    deposit, underlying, and withdraw fixes

--- contracts/CompoundAssetProxy.sol
+++ contracts/CompoundAssetProxy.sol
@@ -6,33 +6,29 @@ import "./interfaces/IERC20.sol";
 import "./interfaces/ICompoundVault.sol";
 import "./WrappedPosition.sol";
 import "./interfaces/CTokenInterfaces.sol";
+import "./interfaces/ComptrollerInterface.sol";
 
 /// @author Element Finance
 /// @title Compound Asset Proxy
 contract CompoundAssetProxy is WrappedPosition {
-    ICompoundVault public immutable vault;
-    uint8 public immutable vaultDecimals;
+    uint8 public immutable underlyingDecimals;
     CTokenInterface public immutable ctoken;
+    ComptrollerInterface public immutable comptroller;
 
     constructor(
-        address vault_,
+        address _ctoken,
+        address _comptroller,
         IERC20 _token,
         string memory _name,
         string memory _symbol
-    ) {
-        // the Yearn asset proxy does interesting things here with WrappedPosition
-
-        // I'm not confident this is how the vault is going to work
-        vault = ICompoundVault(vault_);
-
-        _token.approve(vault_, type(uint256).max);
-
-        // I'm not entirely sure if this should be IERC20 from our interfaces or some
-        // implementation of Compound's CErc20
-        vaultDecimals = IERC20(vault_).decimals();
+    ) WrappedPosition(_token, _name, _symbol) {
+        ctoken = CTokenInterface(_ctoken);
+        comptroller = ComptrollerInterface(_comptroller);
+        _token.approve(ctoken, type(uint256).max);
+        underlyingDecimals = token.decimals();
         require(
-            uint8(_token.decimals()) == vaultDecimals,
-            "Inconsistent decimals"
+            IERC20(ctoken).decimals() == 8,
+            "breaks our assumption in later math"
         );
     }
 
@@ -42,9 +38,17 @@ contract CompoundAssetProxy is WrappedPosition {
         // load balance of contract
         uint256 amount = token.balanceOf(address(this));
 
-        // deposit into compound
-        uint256 share = vault.deposit(amount, address(this));
+        uint256 beforeBalance = ctoken.balanceOf(address(this));
 
+        // deposit into compound
+        uint256 mintStatus = ctoken.mint(amount);
+        require(mintStatus == 0, "compound mint failed");
+
+        uint256 afterBalance = ctoken.balanceOf(address(this));
+
+        // Compound doesn't return this value, so we calculate it manually
+
+        uint256 share = beforeBalance - afterBalance;
         // Return the amount of shares the user has produced, and the amount of underlying used for it.
         return (share, amount);
     }
@@ -52,16 +56,22 @@ contract CompoundAssetProxy is WrappedPosition {
     /// @notice Withdraw the number of shares
     /// @param _shares The number of shares to withdraw
     /// @param _destination The address to send the output funds
-    /// @param _underlyingPerShare The possibly precomputed underlying per share
+    // @param _underlyingPerShare The possibly precomputed underlying per share
     /// @return returns the amount of funds freed by doing a withdraw
+
     function _withdraw(
         uint256 _shares,
         address _destination,
-        uint256 _underlyingPerShare
+        uint256
     ) internal override returns (uint256) {
-        // do the withdraw
-        uint256 amountReceived = vault.withdraw(_shares, address(this), 10000);
+        uint256 beforeBalance = token.balanceOf(address(this));
 
+        // do the withdraw
+        uint256 redeemStatus = ctoken.redeem(_shares);
+        require(redeemStatus == 0, "compound mint failed");
+
+        uint256 afterBalance = token.balanceOf(address(this));
+        uint256 amountReceived = afterBalance - beforeBalance;
         // Transfer the underlying to the destination 'token' is an immutable in WrappedPosition
         token.transfer(_destination, amountReceived);
 
@@ -78,68 +88,10 @@ contract CompoundAssetProxy is WrappedPosition {
         view
         returns (uint256)
     {
-        // TODO: dont' think that input param is right
-        return
-            (_amount * _balanceOfUnderlying(address(this))) /
-            (10**vaultDecimals);
-    }
-
-    //https://github.com/compound-finance/compound-protocol/blob/master/contracts/CToken.sol#L184-L195
-    // /**
-    //  * @notice Get the underlying balance of the `owner`
-    //  * @dev This also accrues interest in a transaction
-    //  * @param owner The address of the account to query
-    //  * @return The amount of underlying owned by `owner`
-    //  */
-    // function balanceOfUnderlying(address owner) external returns (uint) {
-    //     Exp memory exchangeRate = Exp({mantissa: exchangeRateCurrent()});
-    //     (MathError mErr, uint balance) = mulScalarTruncate(exchangeRate, accountTokens[owner]);
-    //     require(mErr == MathError.NO_ERROR, "balance could not be calculated");
-    //     return balance;
-    // }
-
-    //https://github.com/compound-finance/compound-protocol/blob/fcf067f6fa50a93ff9125f5f0abae0ae98d1e8b0/contracts/Comptroller.sol#L1287
-    // /**
-    //  * @notice Claim all comp accrued by the holders
-    //  * @param holders The addresses to claim COMP for
-    //  * @param cTokens The list of markets to claim COMP in
-    //  * @param borrowers Whether or not to claim COMP earned by borrowing
-    //  * @param suppliers Whether or not to claim COMP earned by supplying
-    //  */
-    // function claimComp(address[] memory holders, CToken[] memory cTokens, bool borrowers, bool suppliers) public {
-    //     for (uint i = 0; i < cTokens.length; i++) {
-    //         CToken cToken = cTokens[i];
-    //         require(markets[address(cToken)].isListed, "market must be listed");
-    //         if (borrowers == true) {
-    //             Exp memory borrowIndex = Exp({mantissa: cToken.borrowIndex()});
-    //             updateCompBorrowIndex(address(cToken), borrowIndex);
-    //             for (uint j = 0; j < holders.length; j++) {
-    //                 distributeBorrowerComp(address(cToken), holders[j], borrowIndex);
-    //             }
-    //         }
-    //         if (suppliers == true) {
-    //             updateCompSupplyIndex(address(cToken));
-    //             for (uint j = 0; j < holders.length; j++) {
-    //                 distributeSupplierComp(address(cToken), holders[j]);
-    //             }
-    //         }
-    //     }
-    //     for (uint j = 0; j < holders.length; j++) {
-    //         compAccrued[holders[j]] = grantCompInternal(holders[j], compAccrued[holders[j]]);
-    //     }
-    // }
-
-    /// @notice Get the price per share in the vault
-    /// @return The price per share in units of underlying;
-    function _balanceOfUnderlying(address owner)
-        internal
-        view
-        returns (uint256)
-    {
-        // TODO: add exchange rate details
-        uint256 exchangeRate = ctoken.exchangeRateCurrent();
-        // TODO: it looks like the balanceOfUnderlying might already do the exchange rate?
-
-        return vault.balanceOfUnderlying();
+        // load exchange rate
+        // TODO: doublecheck this is the right function
+        uint256 exchangeRate = ctoken.exchangeRateStored();
+        return // multiply _amount by exchange rate & correct for decimals
+        ((_amount * exchangeRate) / (10**(26 - underlyingDecimals)));
     }
 }

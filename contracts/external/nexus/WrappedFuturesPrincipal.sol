1d3919962435db66dfef96a100c9707df81628c7
SatyamAgrawal <satyam0499@gmail.com>
2021-12-15 12:59:16 +0530

    feat: minor fixes

--- contracts/external/nexus/WrappedFuturesPrincipal.sol
+++ contracts/external/nexus/WrappedFuturesPrincipal.sol
@@ -9,7 +9,6 @@ import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/I
 import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract WrappedFuturesPrincipal is ERC20PermitWithSupply, Authorizable {
-
     using EnumerableSet for EnumerableSet.AddressSet;
     using SafeERC20 for IERC20;
 
@@ -17,8 +16,8 @@ contract WrappedFuturesPrincipal is ERC20PermitWithSupply, Authorizable {
     // Ex - LUSD is used to buy the Curve LUSD v2 yield bearing token
     address public immutable baseToken;
 
-    // Enumerable address list, It contains the list of allowed tranches that are insured by this contract
-    // Criteria to choose the tranches are - 
+    // Enumerable address list, It contains the list of allowed tranches that are covered by this contract
+    // Criteria to choose the tranches are -
     // a). Tranche should have same underlying/base token (i.e ETH, BTC, USDC).
     // b). Should have the similar risk profiles.
     EnumerableSet.AddressSet private _allowedTranches;
@@ -31,53 +30,81 @@ contract WrappedFuturesPrincipal is ERC20PermitWithSupply, Authorizable {
         require(isAllowedTranche(_tranche), "WFP:ALREADY_EXISTS");
         _;
     }
-    
+
     ///@notice Initialize the wrapped token.
     ///@dev    Wrapped token have 18 decimals, It is independent of the baseToken decimals.
     constructor(address baseToken_, address owner_)
-     ERC20Permit(
-        _processName(IERC20Metadata(baseToken_).symbol()),
-        _processSymbol(IERC20Metadata(baseToken_).symbol())
-    ) {
+        ERC20Permit(
+            _processName(IERC20Metadata(baseToken_).symbol()),
+            _processSymbol(IERC20Metadata(baseToken_).symbol())
+        )
+    {
         baseToken = baseToken_;
         _authorize(owner_);
         setOwner(owner_);
     }
 
     ///@notice Allows to create the name for the wrapped token.
-    function _processName(string memory _tokenSymbol) internal pure returns(string memory) {
-        return string(abi.encodePacked("Wrapped", _tokenSymbol, "Futures Principal"));
+    function _processName(string memory _tokenSymbol)
+        internal
+        pure
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked("Wrapped", _tokenSymbol, "Futures Principal")
+            );
     }
 
     ///@notice Allows to create the symbol for the wrapped token.
-    function _processSymbol(string memory _tokenSymbol) internal pure returns(string memory) {
-        return string(abi.encodePacked("ep:","W",_tokenSymbol));
+    function _processSymbol(string memory _tokenSymbol)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string(abi.encodePacked("ep:", "W", _tokenSymbol));
     }
-
 
     /// @notice Add tranches within the allowed tranches enumerable set.
     /// @dev    It is only allowed to execute by the owner of the contract.
     ///         Tranches which has underlying token equals to the base token are
     ///         only allowed to add, Otherwise it will revert.
     /// @param  _tranche Address of the tranche which needs to add.
-    function addTranche(address _tranche) external isValidTranche(_tranche) onlyOwner {
-        require(address(ITranche(_tranche).underlying()) == baseToken, "WFP:INVALID_TRANCHE");
+    function addTranche(address _tranche)
+        external
+        isValidTranche(_tranche)
+        onlyOwner
+    {
+        require(
+            address(ITranche(_tranche).underlying()) == baseToken,
+            "WFP:INVALID_TRANCHE"
+        );
         _allowedTranches.add(_tranche);
         emit TrancheAdded(_tranche);
     }
 
-    /// @notice Allows the defaulter to mint wrapped tokens (Insured position) by
+    /// @notice Allows the defaulter to mint wrapped tokens (Covered position) by
     ///         sending the de-pegged token to the contract.
-    /// @dev    a) Only allow minting the insured position when the tranche got expired otherwise revert.
+    /// @dev    a) Only allow minting the covered position when the tranche got expired otherwise revert.
     ///         b) Sufficient allowance of the principal token (i.e tranche) should be provided
     ///            to the contract by the `msg.sender` to make execution successful.
-    /// @param  _amount Amount of insured position / wrapped token `msg.sender` wants to mint.
-    /// @param  _tranche Address of the tranche which is insured by this insured position contract / wrapped token.
-    function mint(uint256 _amount, address _tranche) external isValidTranche(_tranche) {
+    /// @param  _amount Amount of covered position / wrapped token `msg.sender` wants to mint.
+    /// @param  _tranche Address of the tranche which is covered by this covered position contract / wrapped token.
+    function mint(uint256 _amount, address _tranche)
+        external
+        isValidTranche(_tranche)
+    {
         // Only allow minting when the position get expired.
-        require(ITranche(_tranche).unlockTimestamp() < block.timestamp, "WFP:POSITION_NOT_EXPIRED");
+        require(
+            ITranche(_tranche).unlockTimestamp() < block.timestamp,
+            "WFP:POSITION_NOT_EXPIRED"
+        );
         // Assumed that msg.sender provides the sufficient approval the contract.
-        IERC20(_tranche).safeTransferFrom(msg.sender, address(this), _fromWad(_amount, _tranche));
+        IERC20(_tranche).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _fromWad(_amount, _tranche)
+        );
         // Mint the corresponding wrapped token to the `msg.sender`.
         _mint(msg.sender, _amount);
     }
@@ -85,26 +112,34 @@ contract WrappedFuturesPrincipal is ERC20PermitWithSupply, Authorizable {
     /// @notice Tell whether the given `_tranche` is whitelisted or not.
     /// @param  _tranche Address of the tranche.
     /// @return returns boolean, True -> allowed otherwise false.
-    function isAllowedTranche(address _tranche) public view returns(bool) {
+    function isAllowedTranche(address _tranche) public view returns (bool) {
         return _allowedTranches.contains(_tranche);
     }
 
-    /// @notice Returns the list of tranches that are whitelisted with the contract. 
+    /// @notice Returns the list of tranches that are whitelisted with the contract.
     ///         Order is not maintained.
     /// @return Array of addresses.
-    function allTranches() public view returns(address[] memory) {
+    function allTranches() public view returns (address[] memory) {
         return _allowedTranches.values();
     }
 
-    /// @notice Returns price of the de-pegged token i.e principal token in terms of TBD.
-    function getPrice() external view returns(uint256) {
-        // ... TODO implementation
-        // Need more input from the nexus to validate the requirement.
+    /// @notice Returns price of the de-pegged token i.e principal token in terms of base asset.
+    /// @param  _tranche Address of the tranche, corresponds to which price get queried.
+    /// @return uint256 Price of de-pegged token in terms of base asset.
+    function getPrice(address _tranche) external view returns (uint256) {
+        if (ITranche(_tranche).unlockTimestamp() < block.timestamp) {
+            return uint256(1);
+        } else {
+            revert("No Price until position get matured");
+        }
     }
 
     /// @notice Converts the decimal precision of given `_amount` to `_tranche` decimal.
-    function _fromWad(uint256 _amount, address _tranche) internal view returns(uint256) {
-        return _amount * 10 ** IERC20Metadata(_tranche).decimals() / 1e18;
+    function _fromWad(uint256 _amount, address _tranche)
+        internal
+        view
+        returns (uint256)
+    {
+        return (_amount * 10**IERC20Metadata(_tranche).decimals()) / 1e18;
     }
-
 }
--- contracts/external/nexus/WrappedFuturesPrincipalFactory.sol
+++ contracts/external/nexus/WrappedFuturesPrincipalFactory.sol
@@ -4,14 +4,16 @@ pragma solidity 0.8.0;
 import { WrappedFuturesPrincipal, Authorizable, EnumerableSet } from "./WrappedFuturesPrincipal.sol";
 
 contract WrappedFuturesPrincipalFactory is Authorizable {
-
     using EnumerableSet for EnumerableSet.AddressSet;
 
     // Enumerable list of wrapped tokens that get created from the factory.
     EnumerableSet.AddressSet private _wrappedFuturesPrincipals;
 
     // Emitted when new wrapped principal token get created.
-    event WrappedFuturesPrincipalCreated(address indexed _baseToken, address indexed _owner);
+    event WrappedFuturesPrincipalCreated(
+        address indexed _baseToken,
+        address indexed _owner
+    );
 
     /// @notice Initializing the owner of the contract.
     constructor(address owner_) {
@@ -23,14 +25,31 @@ contract WrappedFuturesPrincipalFactory is Authorizable {
     /// @param  _baseToken Address of the base token / underlying token that is used to buy the wrapped positions.
     /// @param  _owner Address of the owner of wrapped futures.
     /// @return address of wrapped futures token.
-    function create(address _baseToken, address _owner) external onlyOwner returns(address) {
+    function create(address _baseToken, address _owner)
+        external
+        onlyOwner
+        returns (address)
+    {
         // Validate the given params
         _zeroAddressCheck(_owner);
         _zeroAddressCheck(_baseToken);
-        address wfPrincipal = address(new WrappedFuturesPrincipal(_baseToken, _owner));
+        address wfPrincipal = address(
+            new WrappedFuturesPrincipal(_baseToken, _owner)
+        );
         _wrappedFuturesPrincipals.add(wfPrincipal);
         emit WrappedFuturesPrincipalCreated(_baseToken, _owner);
         return wfPrincipal;
+    }
+
+    /// @notice Returns the list of wrapped tokens that are whitelisted with the contract.
+    ///         Order is not maintained.
+    /// @return Array of addresses.
+    function allWrappedFuturesPrincipals()
+        public
+        view
+        returns (address[] memory)
+    {
+        return _wrappedFuturesPrincipals.values();
     }
 
     /// @notice Sanity check for the zero address check.

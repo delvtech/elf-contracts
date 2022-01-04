a9f1d67b9536764ba48a93e8ebabbb641c6497f1
SatyamAgrawal <satyam0499@gmail.com>
2022-01-04 09:47:34 +0530

    interface fixed

--- contracts/external/nexus/WrappedCoveredPrincipalToken.sol
+++ contracts/external/nexus/WrappedCoveredPrincipalToken.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.0;
 import { ERC20PermitWithSupply, ERC20Permit, IERC20Permit } from "../../libraries/ERC20PermitWithSupply.sol";
 import { IWrappedPosition } from "../../interfaces/IWrappedPosition.sol";
 import { ITranche } from "../../interfaces/ITranche.sol";
+import { IWrappedCoveredPrincipalToken } from "./interfaces/IWrappedCoveredPrincipalToken.sol";
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
@@ -11,13 +12,17 @@ import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol"
 
 /// @author Element Finance
 /// @title WrappedCoveredPrincipalToken
-contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
+contract WrappedCoveredPrincipalToken is
+    ERC20PermitWithSupply,
+    AccessControl,
+    IWrappedCoveredPrincipalToken
+{
     using EnumerableSet for EnumerableSet.AddressSet;
     using SafeERC20 for IERC20;
 
     // Address of the base/underlying token which is used to buy the yield bearing token from the wrapped position.
     // Ex - Dai is used to buy the yvDai yield bearing token
-    address public immutable baseToken;
+    address public immutable override baseToken;
 
     // Enumerable address list, It contains the list of allowed wrapped positions that are covered by this contract
     // Criteria to choose the wrapped position are -
@@ -42,16 +47,6 @@ contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
 
     // Emitted when the principal tokens get reclaimed.
     event Reclaimed(address _tranche, uint256 _amount);
-
-    // Memory encoding of the permit data
-    struct PermitData {
-        address spender;
-        uint256 value;
-        uint256 deadline;
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-    }
 
     /// @notice Modifier to validate the wrapped position is whitelisted or not.
     modifier isValidWp(address _wrappedPosition) {
@@ -108,6 +103,7 @@ contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
     /// @param  _wrappedPosition Address of the Wrapped position which needs to add.
     function addWrappedPosition(address _wrappedPosition)
         external
+        override
         isValidWp(_wrappedPosition)
         onlyRole(ADMIN_ROLE)
     {
@@ -132,7 +128,7 @@ contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
         uint256 _expiration,
         address _wrappedPosition,
         PermitData calldata _permitCallData
-    ) external {
+    ) external override {
         require(isAllowedWp(_wrappedPosition), "WFP:INVALID_WP");
         address _tranche = address(
             _deriveTranche(_wrappedPosition, _expiration)
@@ -156,22 +152,34 @@ contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
     /// @notice Tell whether the given `_wrappedPosition` is whitelisted or not.
     /// @param  _wrappedPosition Address of the wrapped position.
     /// @return returns boolean, True -> allowed otherwise false.
-    function isAllowedWp(address _wrappedPosition) public view returns (bool) {
+    function isAllowedWp(address _wrappedPosition)
+        public
+        view
+        override
+        returns (bool)
+    {
         return _allowedWrappedPositions.contains(_wrappedPosition);
     }
 
     /// @notice Returns the list of wrapped positions that are whitelisted with the contract.
     ///         Order is not maintained.
     /// @return Array of addresses.
-    function allWrappedPositions() public view returns (address[] memory) {
+    function allWrappedPositions()
+        external
+        view
+        override
+        returns (address[] memory)
+    {
         return _allowedWrappedPositions.values();
     }
 
     /// @notice Reclaim tranche token (i.e principal token) by the authorized account.
+    /// @dev    Only be called by the address which has the `RECLAIM_ROLE`, Should be Nexus Treasury.
     /// @param  _expiration Timestamp at which the derived tranche would get expired.
     /// @param  _wrappedPosition Address of the Wrapped position which is used to derive the tranche.
     function reclaimPt(uint256 _expiration, address _wrappedPosition)
         external
+        override
         onlyRole(RECLAIM_ROLE)
     {
         require(isAllowedWp(_wrappedPosition), "WFP:INVALID_WP");
@@ -214,7 +222,6 @@ contract WrappedCoveredPrincipalToken is ERC20PermitWithSupply, AccessControl {
     function _deriveTranche(address _position, uint256 _expiration)
         internal
         view
-        virtual
         returns (ITranche)
     {
         bytes32 salt = keccak256(abi.encodePacked(_position, _expiration));
--- contracts/external/nexus/interfaces/IWrappedCoveredPrincipalToken.sol
+++ contracts/external/nexus/interfaces/IWrappedCoveredPrincipalToken.sol
@@ -4,38 +4,55 @@ pragma solidity 0.8.0;
 import { IERC20Permit } from "../../../interfaces/IERC20Permit.sol";
 
 interface IWrappedCoveredPrincipalToken is IERC20Permit {
+    // Memory encoding of the permit data
+    struct PermitData {
+        address spender;
+        uint256 value;
+        uint256 deadline;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
     // Address of the base/underlying token which is used to buy the yield bearing token from the wrapped position.
-    // Ex - LUSD is used to buy the Curve LUSD v2 yield bearing token
+    // Ex - Dai is used to buy the yvDai yield bearing token.
     function baseToken() external view returns (address);
 
-    /// @notice Add tranches within the allowed tranches enumerable set.
+    /// @notice Add wrapped position within the allowed wrapped position enumerable set.
     /// @dev    It is only allowed to execute by the owner of the contract.
-    ///         Tranches which has underlying token equals to the base token are
+    ///         wrapped position which has underlying token equals to the base token are
     ///         only allowed to add, Otherwise it will revert.
-    /// @param  _tranche Address of the tranche which needs to add.
-    function addTranche(address _tranche) external;
+    /// @param  _wrappedPosition Address of the Wrapped position which needs to add.
+    function addWrappedPosition(address _wrappedPosition) external;
 
     /// @notice Allows the defaulter to mint wrapped tokens (Covered position) by
     ///         sending the de-pegged token to the contract.
-    /// @dev    a) Only allow minting the covered position when the tranche got expired otherwise revert.
+    /// @dev    a) Only allow minting the covered position when the derived tranche got expired otherwise revert.
     ///         b) Sufficient allowance of the principal token (i.e tranche) should be provided
     ///            to the contract by the `msg.sender` to make execution successful.
     /// @param  _amount Amount of covered position / wrapped token `msg.sender` wants to mint.
-    /// @param  _tranche Address of the tranche which is covered by this covered position contract / wrapped token.
-    function mint(uint256 _amount, address _tranche) external;
+    /// @param  _expiration Timestamp at which the derived tranche would get expired.
+    /// @param  _wrappedPosition Address of the Wrapped position which is used to derive the tranche.
+    function mint(
+        uint256 _amount,
+        uint256 _expiration,
+        address _wrappedPosition,
+        PermitData calldata _permitCallData
+    ) external;
 
-    /// @notice Tell whether the given `_tranche` is whitelisted or not.
-    /// @param  _tranche Address of the tranche.
+    /// @notice Tell whether the given `_wrappedPosition` is whitelisted or not.
+    /// @param  _wrappedPosition Address of the wrapped position.
     /// @return returns boolean, True -> allowed otherwise false.
-    function isAllowedTranche(address _tranche) external view returns (bool);
+    function isAllowedWp(address _wrappedPosition) external view returns (bool);
 
-    /// @notice Returns the list of tranches that are whitelisted with the contract.
+    /// @notice Returns the list of wrapped positions that are whitelisted with the contract.
     ///         Order is not maintained.
     /// @return Array of addresses.
-    function allTranches() external view returns (address[] memory);
+    function allWrappedPositions() external view returns (address[] memory);
 
-    /// @notice Returns price of the de-pegged token i.e principal token in terms of base asset.
-    /// @param  _tranche Address of the tranche, corresponds to which price get queried.
-    /// @return uint256 Price of de-pegged token in terms of base asset.
-    function getPrice(address _tranche) external view returns (uint256);
+    /// @notice Reclaim tranche token (i.e principal token) by the authorized account.
+    /// @dev    Only be called by the address which has the `RECLAIM_ROLE`, Should be Nexus Treasury.
+    /// @param  _expiration Timestamp at which the derived tranche would get expired.
+    /// @param  _wrappedPosition Address of the Wrapped position which is used to derive the tranche.
+    function reclaimPt(uint256 _expiration, address _wrappedPosition) external;
 }

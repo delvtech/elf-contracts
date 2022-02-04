6e195ec1cc55b8b1eb245766fb264e247d99c6ca
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-02-04 00:04:21 +0000

    License

--- contracts/interfaces/IAsset.sol
+++ contracts/interfaces/IAsset.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 // This interface is used instead of importing one from balancer contracts to
--- contracts/interfaces/ICurvePool.sol
+++ contracts/interfaces/ICurvePool.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 interface ICurvePool {
@@ -5,11 +6,19 @@ interface ICurvePool {
         uint256 amount,
         uint256 idx,
         uint256 minAmount
-    ) external;
+    ) external payable;
 
     function remove_liquidity_one_coin(
         uint256 amount,
         int128 idx,
         uint256 minAmount
-    ) external;
+    ) external payable;
+
+    function add_liquidity(uint256[2] memory amountCtx, uint256 minAmount)
+        external
+        payable;
+
+    function add_liquidity(uint256[3] memory amountCtx, uint256 minAmount)
+        external
+        payable;
 }

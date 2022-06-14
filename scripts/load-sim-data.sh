ef2f94e4831f7a364eab3bdd8ef2a44a85c76112
mihai6 <mihai@element.fi>
2022-06-14 14:07:58 -0400

    check for python3 executable

--- scripts/load-sim-data.sh
+++ scripts/load-sim-data.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+version=$(python -V 2>&1 | grep -Po '(?<=Python )\d')
+if [[ "$version" = 3 ]]
+then
+    echo "Valid version"
+    alias py="python"
+else
+    echo "Invalid version"
+    alias py="python3"
+fi
+
 rm -rf test/simulation/analysis
 
 echo "Downloading analysis repo..."
 git clone https://github.com/element-fi/analysis.git ./test/simulation/analysis
 
 echo "Generate sim data"
-python3 ./test/simulation/analysis/scripts/TestTradesSim.py
+py ./test/simulation/analysis/scripts/TestTradesSim.py
 
 mv testTrades.json ./test/simulation/
 echo "Done!"
